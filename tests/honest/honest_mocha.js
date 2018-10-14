d3d714eaa8c777395da73e9163da7f4eb0e28ccc
James Wang <jamesnewking@gmail.com>
2018-10-13 18:22:32 -0700

    fully running smoke test on honest site

--- tests/honest/honest_mocha.js
+++ tests/honest/honest_mocha.js
@@ -80,31 +80,11 @@ describe( 'Honest smoke test', () => {
         };
     } );
 
-    // after(async () => {
-    //     await driver.quit();
-    //
-    // });
+    after(async () => {
+        await driver.quit();
 
-      // it(`hover mouse over auctions`, async () => {
-    //     let auctionsToptab = await driver.findElement({'xpath': xomeXpath.auctionsToptab});
-    //     let searchAuctions = await driver.findElement({'xpath': xomeXpath.searchAuctions});
-    //     let allHomes       = await driver.findElement({'xpath': xomeXpath.allHomes});
-    //
-    //     const actions = driver.actions({bridge:true});
-    //     await actions.move({duration:1500,origin:auctionsToptab,x:0,y:0}).perform();
-    //     await actions.move({duration:1500,origin:searchAuctions,x:0,y:0}).perform();
-    //     await actions.move({duration:1500,origin:allHomes,x:0,y:0}).perform();
-    //     await allHomes.click();
-    // });
-    //
-    // it('back to main Xome page', async () => {
-    //     await driver.findElement({'xpath': xomeXpath.mainLogo}).click();
-    //     await driver.wait(until.elementLocated({'xpath': xomeXpath.mainSlogan}));
-    //     const mainSlogan = await driver.findElement({'xpath': xomeXpath.mainSlogan}).getText();
-    //     //console.log(`Main page slogan is: ${mainSlogan}`);
-    //     expect(mainSlogan).to.equal(xomeXpath.text.textSlogan,'Error: slogan does not match');
-    // });
-    //
+    });
+
     // it('all img URLs are valid links, returns server code 200', async () => {
     //     let imageArr = await driver.findElements({'css': 'img'});
     //     let linkArr = await driver.findElements({'css' : 'a'});
@@ -137,14 +117,6 @@ describe( 'Honest smoke test', () => {
 
     it('close splash ad', async() => {
         await driver.sleep(3000);
-        // let splash1 = await driver.findElement( { 'xpath' : honestXpath.close_splash_ad1} ).isDisplayed();
-        // if (splash1){
-        //     console.log("splash1 appeared!");
-        //     await driver.findElement( { 'xpath' : honestXpath.close_splash_ad1} ).click();
-        // } else {
-        //     console.log("splash2 appeared!");
-        //     await driver.findElement( { 'xpath' : honestXpath.close_splash_ad2} ).click();
-        // }
 
         try {
             let splash1 = await driver.findElement( { 'xpath' : honestXpath.close_splash_ad1} ).isDisplayed();
@@ -170,67 +142,102 @@ describe( 'Honest smoke test', () => {
         await driver.findElement( { 'xpath' : honestXpath.topNavDiapering } ).click();
     });
 
-    it('all img URLs are valid links, returns server code 200', async () => {
-            let imageArr = await driver.findElements({'css': 'img'});
-            let linkArr = await driver.findElements({'css' : 'a'});
-            console.log(`number of <img> tags: ${await imageArr.length}`);
-            console.log(`number of <a> tags: ${await linkArr.length}`);
-            await driver.executeScript( "window.scrollTo(0,3000);" );
-            await driver.executeScript( "window.scrollTo(0,-3000);" );
-            for (let i = 0; i < imageArr.length; i++) {
-                //console.log( await imageArr[i].getAttribute('src'));
-                let imgUrl;
-                try {
-                    imgUrl = await imageArr[i].getAttribute('src');
-                }
-                catch(err){
-                    console.log("something wrong with this imgUrl #", i);
-                    continue;
-                }
-
-                if (imgUrl == null){
-                    console.log(`this imgUrl #${i} is null!`);
-                    continue;
-                }
-
-                // try {
-                //     expect(imgUrl).to.exist;
-                // }
-                // catch(err) {
-                //     console.log(`this imgUrl: does not exist`);
-                //     continue;
-                // }
-                expect(imgUrl).to.be.a("string");//.catch(() => console.log(`this imgUrl: ${imgUrl} is not a string`));
-                console.log(`${i}) `,imgUrl);
-                fetch(imgUrl)
-                    .then(
-                        (response) => {
-                            if (response.status === 200) {
-                                console.log(`${i})image link to ${imgUrl} is valid`);
-                                return;
-                            }
-                        }
-                    )
-                    .catch(
-                        (err) => console.log('the image link has error: ', err)
-                    );
-            }
-        });
+    // it('all img URLs are valid links, returns server code 200', async () => {
+    //         let imageArr = await driver.findElements({'css': 'img'});
+    //         let linkArr = await driver.findElements({'css' : 'a'});
+    //         console.log(`number of <img> tags: ${await imageArr.length}`);
+    //         console.log(`number of <a> tags: ${await linkArr.length}`);
+    //         await driver.executeScript( "window.scrollTo(0,3000);" );
+    //         await driver.executeScript( "window.scrollTo(0,-3000);" );
+    //         for (let i = 0; i < imageArr.length; i++) {
+    //             //console.log( await imageArr[i].getAttribute('src'));
+    //             let imgUrl;
+    //             try {
+    //                 imgUrl = await imageArr[i].getAttribute('src');
+    //             }
+    //             catch(err){
+    //                 console.log("something wrong with this imgUrl #", i);
+    //                 continue;
+    //             }
+    //
+    //             if (imgUrl == null){
+    //                 console.log(`this imgUrl #${i} is null!`);
+    //                 continue;
+    //             }
+    //
+    //             // try {
+    //             //     expect(imgUrl).to.exist;
+    //             // }
+    //             // catch(err) {
+    //             //     console.log(`this imgUrl: does not exist`);
+    //             //     continue;
+    //             // }
+    //             expect(imgUrl).to.be.a("string");//.catch(() => console.log(`this imgUrl: ${imgUrl} is not a string`));
+    //             console.log(`${i}) `,imgUrl);
+    //             fetch(imgUrl)
+    //                 .then(
+    //                     (response) => {
+    //                         if (response.status === 200) {
+    //                             console.log(`${i})image link to ${imgUrl} is valid`);
+    //                             return;
+    //                         }
+    //                     }
+    //                 )
+    //                 .catch(
+    //                     (err) => console.log('the image link has error: ', err)
+    //                 );
+    //         }
+    //     });
 
     it('add feeding to cart', async () => {
         await driver.sleep(1000);
         await driver.findElement( { 'xpath' : honestXpath.topNavFeeding } ).click();
         await driver.sleep(1000);
+        await driver.executeScript( "window.scrollTo(0,300);" );
         await driver.findElement( { 'xpath' : honestXpath.premiumInfantFormula.xpath}).click().catch( ()=> console.log('did not find premium baby formula') );
         await driver.sleep(1000);
         let singleItemTitle = await driver.findElement( { 'xpath' : honestXpath.singlePremiumInfantFormula}).getText();
         console.log(`the item title is: ${singleItemTitle}`);
         let itemDropDown = await driver.findElement( { 'xpath' : honestXpath.singlePremiumInfantFormulaSizeDropDown} );
-        await itemDropDown.click();
         const actions = driver.actions({bridge:true});
-        await actions.move({duration:1500,origin:itemDropDown,x:50,y:0}).click().perform();
+        await actions.click(itemDropDown).sendKeys( webdriver.Key.ARROW_DOWN ).perform();
+        await driver.sleep(1000);//needed for firefox
+        await actions.sendKeys(webdriver.Key.ENTER).perform();
+        // await actions.move({duration:1500,origin:itemDropDown,x:20,y:-100}).click().perform();
+        let itemPrice = await driver.findElement( {'xpath' : honestXpath.singlePremiumInfantFormulaPrice} ).getText();
+        itemPrice = "$" + itemPrice;
+        await driver.findElement( {'xpath' : honestXpath.singlePremiumInfantFormulaBuyNow} ).click();
+        console.log(`the price is ${itemPrice}`);
+        await driver.findElement( {'xpath' : honestXpath.singlePremiumInfantFormulaCart} ).click();
+
+        await driver.wait(until.elementLocated( { 'xpath' : honestXpath.cart1stItemName } ) );
+        let cartItemName = await driver.findElement( {'xpath' : honestXpath.cart1stItemName}).getText();
+        let cartItemPrice = await driver.findElement( {'xpath' : honestXpath.cart1stItemPrice}).getText();
+        console.log(`The item name ${cartItemName} is ${cartItemPrice}`);
+
         expect(singleItemTitle).to.equal(honestXpath.premiumInfantFormula.name,'Error: product name does not match!');
+        expect(singleItemTitle).to.equal(cartItemName,'Error: product name does not match in the cart!');
+        expect(itemPrice).to.equal(cartItemPrice,'Error: product price does not match in the cart!');
     });
+
+    it('empty cart', async () => {
+        await driver.findElement( {'xpath': honestXpath.cartEmptyItems} ).click();
+        let emptyCartMsg = await driver.findElement( { 'xpath': honestXpath.cartEmptyMessage } ).getText();
+        let emptyCartPrice = await driver.findElement( { 'xpath': honestXpath.cartEmptySubtotal } ).getText();
+        console.log(`The cart is saying ${emptyCartMsg} with a subtotal of ${emptyCartPrice}`);
+        expect(emptyCartMsg).to.equal(honestXpath.text.emptyCartMsg,'Error: empty cart message does not match!');
+        expect(emptyCartPrice).to.equal(honestXpath.text.emptyCartTotal,'Error: empty cart message does not match!');
+    });
+
+    it('back to main page', async () => {
+        let currentTitle = await driver.findElement( {'xpath': honestXpath.cartPageTitle} ).getText();
+        await driver.findElement( {'xpath': honestXpath.topLeftMainLogo} ).click();
+        await driver.wait(until.elementLocated( { 'xpath' : honestXpath.topNavFeeding }) );
+        let topNavFeeding = await driver.findElement( {'xpath': honestXpath.topNavFeeding } );
+        console.log(`Did we go back to the main page? ${await topNavFeeding.isDisplayed()}`);
+        expect(await topNavFeeding.isDisplayed(),'Error: Did not get back to main page').to.be.true;
+    });
+
 
     // it('add personal care to cart', async () => {
     //     await driver.sleep(1000);
@@ -247,10 +254,6 @@ describe( 'Honest smoke test', () => {
 //         await actions.move({duration:1500,origin:sign_in,x:0,y:0}).perform();
 //         console.log('moving mouse');
 //         await sign_in.click();
-//
-// //http://scraping.pro/recaptcha-solve-selenium-python/
-// //https://www.packtpub.com/mapt/book/web_development/9781783981922/3/ch03lvl1sec38/simulating-scrolling-in-phantomjs
-//
 //
 //         //let loginFrame = await driver.findElement(By.id('login-iframe'));
 //         //await driver.switchTo().frame(loginFrame).catch( ()=> console.log('did not switch to active element') );
@@ -279,81 +282,5 @@ describe( 'Honest smoke test', () => {
 //
 //
 //     })
-
-    // it('verify the login name is correct', async () => {
-    //     await driver.switchTo().defaultContent().catch( ()=> console.log('can not find defaultContent window'));//need this for firefox
-    //     await driver.wait(until.elementLocated( { 'xpath' : xomeXpath.loginName } ) );
-    //     let loginName = await driver.findElement({ 'xpath': xomeXpath.loginName }).getText();
-    //     expect(loginName.toUpperCase()).to.equal(credentials.loginName, 'Error: login name does not match');
-    // })
-    //
-    // it(`search homes in ${credentials.city}`, async () => {
-    //     await driver.findElement( { 'id' : 'homepage-search-field' } ).clear();
-    //     await driver.findElement( { 'id' : 'homepage-search-field' } ).sendKeys(credentials.city);
-    //     await driver.findElement( { 'className' : 'search-field-button'   } ).click();
-    //     await driver.sleep( 1000 );
-    //     ////*[@id="location-criteria-list"]/ul/li/a
-    //     if( browserToTest==='edge' ){
-    //         await driver.wait(until.elementLocated( { 'xpath' : xomeXpath.cityName } ) );
-    //     };
-    //     let cityName = await driver.findElement( { 'xpath': xomeXpath.cityName }).getText();
-    //     expect(cityName).to.equal(credentials.cityName, 'Error: city name does not match');
-    //
-    // })
-    //
-    // it(`verify the number of properties on the page`, async () => {
-    //     await driver.wait(until.elementLocated( { 'xpath' : xomeXpath.firstPropImg } ) );
-    //     let properties = await driver.findElements( { 'xpath' : xomeXpath.firstPropImg } );
-    //     console.log(`Number of properties on the page: ${properties.length}`);
-    //     expect(properties.length).to.equal(24, 'Error: number of properties on the page is not 24');
-    // })
-    //
-    // it(`click through the pictures on each of the properties`, async () => {
-    //     let properties = await driver.findElements( { 'xpath' : xomeXpath.firstPropImg } );
-    //
-    //     if( browserToTest !== 'firefox' ) {
-    //         for (let pInPage = 1; pInPage <= properties.length; pInPage++) {
-    //             await driver.sleep(1500);
-    //             await driver.findElement({'xpath': `//*[@id="mapsearch-results-body"]/div[${pInPage}]`}).catch(() => console.log(`can not find the ${pInPage}th property!`));//click on the first property listed
-    //             await driver.findElement({'xpath': `//*[@id="mapsearch-results-body"]/div[${pInPage}]`}).click();//click on the property listed
-    //             await driver.wait(until.elementLocated({'xpath': xomeXpath.picTotal }));//make sure this is correct
-    //             await driver.sleep(1000);
-    //             let picTotal = await driver.findElement({'xpath': xomeXpath.picTotal }).getText();//number of property pictures
-    //             if (picTotal === ''){
-    //                 picTotal = 'no'; //in case of no pictures shown
-    //             };
-    //             let streetAddr  = await driver.findElement({'xpath': xomeXpath.streetAddr  }).getText();//street address
-    //             let cityAddr    = await driver.findElement({'xpath': xomeXpath.cityAddr    }).getText();//city address
-    //             let askingPrice = await driver.findElement({'xpath': xomeXpath.askingPrice }).getText();//property asking price
-    //             expect(cityAddr).to.equal(xomeXpath.text.cityText, `Error: City name does not match ${xomeXpath.cityText}`);
-    //             console.log(`The property at ${streetAddr} ${cityAddr} has asking price of ${askingPrice} with ${picTotal} property pictures`);
-    //             if (picTotal > 1) {
-    //                 for (let i = 0; i < picTotal; i++) {
-    //                     if (browserToTest==='edge'){
-    //                         await driver.sleep(1500);
-    //                     };
-    //                     await driver.findElement({'xpath': xomeXpath.nextPic }).click().catch(() => console.log('nope, could not find next pic >'));
-    //                     if (i > 3) {
-    //                         i = picTotal;//So we don't click through all of the pictures, because there could be a lot of pictures!
-    //                     }
-    //                 }
-    //             };
-    //             await driver.findElement({'xpath': xomeXpath.closeModal }).click();//close modal
-    //             if (pInPage > 5) {
-    //                 pInPage = properties.length;//so we don't click through all of the properties
-    //             }
-    //         }
-    //     }
-    // })
-    //
-    // if (browserToTest !== '--edge') { //edge bug where can not click on logout because out of viewable screen
-    //     it('logout of Xome', async () => {
-    //         await driver.switchTo().defaultContent().catch(() => console.log('can not find defaultContent window'));//for Edge
-    //         await driver.wait(until.elementLocated({'xpath': xomeXpath.menuDropDown}));//menu dropdown
-    //         await driver.findElement({'id': xomeXpath.navMenuButton}).click();
-    //         await driver.sleep(1500);
-    //         await driver.findElement({'xpath': xomeXpath.userLogout}).click();//logout; edge can not see the userLogout
-    //     })
-    // }
 });
 
