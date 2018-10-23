d357138e3d677b4ac0d8241fb90976c41e4cee85
James Wang <jamesnewking@gmail.com>
2018-10-23 15:06:25 -0700

    changed code to support edge and also added --checkpix option

--- tests/hundreds_POM/hundreds_pom.js
+++ tests/hundreds_POM/hundreds_pom.js
@@ -7,6 +7,7 @@ const webdriver = require('selenium-webdriver'),
 const { expect } = require('chai');
 var driver;
 let browserToTest = process.argv[4];
+let checkPix = process.argv[5];
 const chrome = require('selenium-webdriver/chrome');
 const firefox = require('selenium-webdriver/firefox');
 const screenSize = {
@@ -20,21 +21,18 @@ const shopitem = require("./pom_shopitem");
 
 describe( 'The Hundreds smoke test', () => {
     before(async () => {
-
         switch(browserToTest) {
             case '--edge':
                 driver = await new webdriver.Builder()
                     .usingServer('http://localhost:4444/wd/hub')
                     .withCapabilities(webdriver.Capabilities.edge())
                     .build();
-                browserToTest = 'edge';
                 break;
             case '--firefox':
                 driver = await new webdriver.Builder()
                     .usingServer('http://localhost:4444/wd/hub')
                     .withCapabilities(webdriver.Capabilities.firefox())
                     .build();
-                browserToTest = 'firefox';
                 break;
             case '--headlessfirefox':
                 driver = await new webdriver
@@ -42,7 +40,6 @@ describe( 'The Hundreds smoke test', () => {
                     .withCapabilities(webdriver.Capabilities.firefox())
                     .setFirefoxOptions(new firefox.Options().headless().windowSize(screenSize))
                     .build();
-                browserToTest = 'firefox';
                 console.log('headless FireFox mode');
                 break;
             case '--ie':
@@ -50,7 +47,6 @@ describe( 'The Hundreds smoke test', () => {
                     .usingServer('http://localhost:4444/wd/hub')
                     .withCapabilities(webdriver.Capabilities.ie())
                     .build();
-                browserToTest = 'ie';
                 break;
             case '--headlesschrome':
                 driver = await new webdriver.Builder()
@@ -58,16 +54,18 @@ describe( 'The Hundreds smoke test', () => {
                     .withCapabilities(webdriver.Capabilities.chrome())
                     .setChromeOptions(new chrome.Options().headless().windowSize(screenSize))
                     .build();
-                browserToTest = 'chrome';
                 console.log('headless Chrome mode');
                 break;
             default:
+                let options = new chrome.Options();
+                options.setUserPreferences({'profile.default_content_setting_values.notifications': 2});
                 driver = await new webdriver.Builder()
                     .usingServer('http://localhost:4444/wd/hub')
                     .withCapabilities(webdriver.Capabilities.chrome())
+                    .setChromeOptions(options)
                     .build();
-                browserToTest = 'chrome';
         }
+        browserToTest = browserToTest.slice(2);
         console.log(`Testing browser: ${browserToTest}`);
         await driver.get('https://thehundreds.com/');
         await driver.manage().setTimeouts({implicit:10000});
@@ -87,65 +85,82 @@ describe( 'The Hundreds smoke test', () => {
     it(`(1) testing webpage title`, async () => {
         let webTitle = new title(driver);
         expect(webTitle.expectedTitleName).to.equal(await webTitle.actualWebTitle(), 'Error: main webpage title does not match')
-
     })
 
 
-    // it('(2) all img URLs are valid links, returns server code 200', async () => {
-    //         let imageArr = await driver.findElements({'css': 'img'});
-    //         let linkArr = await driver.findElements({'css' : 'a'});
-    //         console.log(`number of <img> tags: ${await imageArr.length}`);
-    //         console.log(`number of <a> tags: ${await linkArr.length}`);
-    //         await driver.executeScript( "window.scrollTo(0,30000);" );
-    //         await driver.executeScript( "window.scrollTo(0,-30000);" );
-    //         for (let i = 0; i < imageArr.length; i++) {
-    //             //console.log( await imageArr[i].getAttribute('src'));
-    //             let imgUrl;
-    //             try {
-    //                 imgUrl = await imageArr[i].getAttribute('src');
-    //             }
-    //             catch(err){
-    //                 console.log("something wrong with this imgUrl #", i);
-    //                 continue;
-    //             }
-    //
-    //             if (imgUrl == null){
-    //                 console.log(`this imgUrl #${i} is null!`);
-    //                 continue;
-    //             }
-    //
-    //             // try {
-    //             //     expect(imgUrl).to.exist;
-    //             // }
-    //             // catch(err) {
-    //             //     console.log(`this imgUrl: does not exist`);
-    //             //     continue;
-    //             // }
-    //             expect(imgUrl).to.be.a("string");//.catch(() => console.log(`this imgUrl: ${imgUrl} is not a string`));
-    //             console.log(`${i}) `,imgUrl);
-    //             fetch(imgUrl)
-    //                 .then(
-    //                     (response) => {
-    //                         if (response.status === 200) {
-    //                             console.log(`${i})image link to ${imgUrl} is valid`);
-    //                             return;
-    //                         }
-    //                     }
-    //                 )
-    //                 .catch(
-    //                     (err) => console.log('the image link has error: ', err)
-    //                 );
-    //         }
-    //     });
+    it('(2) all img URLs are valid links, returns server code 200', async () => {
+        if(checkPix==='--checkpix') {
+            let imageArr = await driver.findElements({'css': 'img'});
+            let linkArr = await driver.findElements({'css': 'a'});
+            console.log(`number of <img> tags: ${await imageArr.length}`);
+            console.log(`number of <a> tags: ${await linkArr.length}`);
+            await driver.executeScript("window.scrollTo(0,30000);");
+            await driver.sleep(1500);
+            await driver.executeScript("window.scrollTo(0,-30000);");
+            let badPictureLink = false;
+            for (let i = 0; i < imageArr.length; i++) {
+                //console.log( await imageArr[i].getAttribute('src'));
+                let imgUrl;
+                try {
+                    imgUrl = await imageArr[i].getAttribute('src');
+                }
+                catch (err) {
+                    console.log("something wrong with this imgUrl #", i);
+                    badPictureLink = true;
+                    continue;
+                }
+
+                if (imgUrl == null) {
+                    console.log(`this imgUrl #${i} is null!`);
+                    continue;
+                }
+
+                // try {
+                //     expect(imgUrl).to.exist;
+                // }
+                // catch(err) {
+                //     console.log(`this imgUrl: does not exist`);
+                //     continue;
+                // }
+                expect(imgUrl).to.be.a("string");//.catch(() => console.log(`this imgUrl: ${imgUrl} is not a string`));
+                console.log(`${i}) `, imgUrl);
+                fetch(imgUrl)
+                    .then(
+                        (response) => {
+                            if (response.status === 200) {
+                                console.log(`${i})image link to ${imgUrl} is valid`);
+                                return;
+                            }
+                        }
+                    )
+                    .catch(
+                        (err) => console.log('the image link has error: ', err)
+                    );
+
+            }
+            await driver.sleep(6000);
+            expect( badPictureLink ).to.equal( false ,'There is at lease one img with a bad link');
+        } else {
+            console.log('--Checkpix did not run.');
+        }
+        });
 
 
 
     it('(3) click shop on Nav', async () => {
-        await driver.findElement( hundPath.topNavHamburger ).click();
-        await driver.findElement( hundPath.topNavShop ).click();
+        if(browserToTest==='edge'){
+            console.log('edge');
+            await driver.findElement( hundPath.edgeTopNavShop ).click();
+        } else {
+            await driver.findElement( hundPath.topNavHamburger ).click();
+            //await driver.findElement( hundPath.topNavHamburger ).click();
+            //await driver.findElement( hundPath.topNavHamburger ).click();
+            await driver.wait(until.elementLocated( hundPath.topNavShop )).isDisplayed();
+            await driver.findElement( hundPath.topNavShop ).click();
+        }
     });
 
-    it('(4) add item to cart', async () => {
+    it('(4) add item to cart, verify product in cart', async () => {
         //await driver.switchTo().defaultContent().catch( ()=> console.log('can not find defaultContent window'));
         let shopItem = new shopitem(driver, webdriver);
         let gridItem = await shopItem.addOneItem();
